1bd62e4ae9fdea7d032db4d2a3f289658a826069
David, Jian <david.jian@myre.life>
2020-04-08 16:34:57 +0800

    feat: [Kabu] 加入預測公式 📈

--- src/components/TurnipLineChart.vue
+++ src/components/TurnipLineChart.vue
@@ -1,11 +1,28 @@
 <template>
-  <canvas :id="canvasId"></canvas>
+  <div class="chart">
+    <div class="text-center margin-bt-5">
+      <span class="margin-lr-5" v-for="item in patternsPercentage" :key="item.id">
+        <span>{{ item.label }}</span>
+        <span class="little-text">{{ item.percentage }}%</span>
+      </span>
+    </div>
+    <div v-if="isShort" class="text-center little-text margin-b-5">數據不足，預測結果僅供參考</div>
+    <canvas :id="canvasId"></canvas>
+  </div>
 </template>
 
 <script>
 import Chart from 'chart.js';
+import _zip from 'lodash/zip';
 
 import { momentUtil } from '@/utils';
+import {
+  possiblePatterns,
+  patternReducer,
+  averageReducer,
+  minWeekReducer,
+  patternMapping,
+} from '@/utils/predictions.js';
 
 export default {
   props: {
@@ -48,10 +65,47 @@ export default {
     datasetData() {
       const tempArr = [];
       Object.keys(this.sellPrice).forEach(key => {
-        tempArr.push(this.sellPrice[key].am || 0);
-        tempArr.push(this.sellPrice[key].pm || 0);
+        tempArr.push(+this.sellPrice[key].am || 0);
+        tempArr.push(+this.sellPrice[key].pm || 0);
       });
       return tempArr;
+    },
+    possibilities() {
+      const filter = [+this.buyPrice, ...this.datasetData.map(num => num || undefined)];
+
+      let patterns = possiblePatterns(filter);
+      const patternCount = patterns.reduce((acc, cur) => acc + cur.length, 0);
+      if (patternCount === 0) patterns = possiblePatterns([0, ...filter.slice(1)]);
+
+      const minMaxPattern = patternReducer(patterns);
+      const minMaxData = _zip(...minMaxPattern);
+      const avgPattern = patternReducer(patterns, averageReducer);
+      const avgData = _zip(...avgPattern);
+      const [minWeekValue] = patternReducer(patterns, minWeekReducer);
+
+      return {
+        patterns,
+        minMaxData,
+        minWeekValue,
+      };
+    },
+    patternsPercentage() {
+      const { patterns } = this.possibilities;
+
+      const total = patterns.reduce((pre, cur) => (pre += cur.length), 0);
+
+      return patterns
+        .map((p, i) => {
+          return {
+            id: i,
+            label: patternMapping[i],
+            percentage: p.length ? Math.round((p.length / total) * 100) : 0,
+          };
+        })
+        .filter(item => item.percentage);
+    },
+    isShort() {
+      return this.datasetData.filter(v => v > 0).length < 2;
     },
   },
   watch: {
@@ -79,15 +133,44 @@ export default {
           labels: this.labels,
           datasets: [
             {
-              label: '賣出價格',
-              borderColor: '#00c300',
-              data: this.datasetData,
-            },
-            {
               label: '買入價格',
               borderColor: '#ffffff',
-              pointStyle: 'line',
               data: Array(12).fill(this.buyPrice),
+              pointRadius: 0,
+              pointHoverRadius: 0,
+              borderDash: [2, 5],
+            },
+            // {
+            //   label: '保證價格',
+            //   data: new Array(12).fill(this.possibilities?.minWeekValue || null),
+            //   fill: true,
+            //   borderColor: '#007D75',
+            //   pointRadius: 0,
+            //   pointHoverRadius: 0,
+            //   borderDash: [3, 6],
+            // },
+            {
+              label: '每日賣價',
+              borderColor: '#EF8341',
+              // backgroundColor: '#EF8341',
+              data: this.datasetData.filter(num => !!num),
+            },
+            {
+              label: '預測最高賣價',
+              borderColor: '#A5D5A5',
+              backgroundColor: '#A5D5A5',
+              data: this.possibilities?.minMaxData[1] ?? [],
+              pointRadius: 0,
+              pointHoverRadius: 0,
+              fill: 3,
+            },
+            {
+              label: '預測最低賣價',
+              borderColor: '#88C9A1',
+              backgroundColor: '#88C9A1',
+              data: this.possibilities?.minMaxData[0] ?? [],
+              pointRadius: 0,
+              pointHoverRadius: 0,
             },
           ],
         },
@@ -96,13 +179,17 @@ export default {
           legend: {
             display: true,
           },
+          tooltips: {
+            intersect: false,
+            mode: 'index',
+          },
           scales: {
             yAxes: [
               {
                 display: true,
                 ticks: {
                   min: 0,
-                  stepSize: 5, // <----- This prop sets the stepSize
+                  stepSize: 50, // <----- This prop sets the stepSize
                 },
               },
             ],
@@ -115,4 +202,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.chart {
+  max-width: 800px;
+  width: 100%;
+  margin: 0 auto;
+}
 </style>
