39acd9a7fd77e0ad5607f71a3a0da26dce7b0b98
clipwww <clipwww@gmail.com>
2020-07-31 18:06:08 +0800

    feat: [🌻] add 彈幕列表

--- src/views/Himawari/Details.vue
+++ src/views/Himawari/Details.vue
@@ -32,7 +32,7 @@
           <!-- <div v-if="imgSrc" class="text-center margin-b-10">
             <van-image :src="imgSrc"></van-image>
           </div>-->
-          <span class="little-text">{{ item.author_comment }}</span>
+          <div class="little-text margin-t-10">{{ item.author_comment }}</div>
           <!-- <van-divider></van-divider>
           <span class="little-text">{{ item.user_comment }}</span>-->
         </div>
@@ -42,7 +42,16 @@
             <van-tag type="default">類別: {{ item.author_tag }}</van-tag>
             <van-tag type="default" class="margin-l-10">長度: {{ formatTime }}</van-tag>
           </div>
-          <van-button type="info" size="small" plain @click="goHimawari">前往原頁面</van-button>
+          <div>
+            <van-button
+              class="padding-lr-5 margin-r-5"
+              type="primary"
+              size="mini"
+              plain
+              @click="showPopup = true"
+            >彈幕列表</van-button>
+            <van-button class="padding-lr-5" type="info" size="mini" plain @click="goHimawari">前往原頁面</van-button>
+          </div>
         </div>
       </van-panel>
       <van-cell is-link center clickable @click="openUrl(item)">
@@ -69,17 +78,60 @@
         </div>
         <div slot="label">{{ s.adddate | formatDate }}</div>
       </van-cell>
+
+      <van-popup
+        v-model="showPopup"
+        round
+        position="bottom"
+        close-on-popstate
+        :style="{ height: '87%' }"
+      >
+        <!-- <table class="danmaku-table">
+          <tbody>
+             <tr v-for="item in comments" :key="item.no">
+                <td class="little-text">{{ item.digital_time }}</td>
+                <td>{{ item.msg }}</td>
+                <td class="little-text">{{ item.id }}</td>
+              </tr>
+            
+          </tbody>
+        </table>-->
+        <DynamicScroller class="scroller" :items="comments" key-field="no" :min-item-size="33">
+          <template v-slot="{ item, index, active }">
+            <DynamicScrollerItem
+              :item="item"
+              :active="active"
+              :size-dependencies="[
+                item.msg,
+              ]"
+              :data-index="index"
+            >
+              <div class="scroller__item">
+                <div class="little-text">{{ item.digital_time }}</div>
+                <div class="scroller__item__msg">{{ item.msg }}</div>
+                <!-- <div class="little-text">{{ item.id }}</div> -->
+              </div>
+            </DynamicScrollerItem>
+          </template>
+        </DynamicScroller>
+      </van-popup>
     </template>
   </div>
 </template>
 
 <script>
 import moment from 'moment';
-import Danmaku from 'danmaku/dist/esm/danmaku.canvas.js';
+import Danmaku from 'danmaku';
+import { DynamicScroller, DynamicScrollerItem } from 'vue-virtual-scroller';
+import 'vue-virtual-scroller/dist/vue-virtual-scroller.css';
 
 import { himawariSVC } from '@/services';
 
 export default {
+  components: {
+    DynamicScroller,
+    DynamicScrollerItem,
+  },
   metaInfo() {
     return {
       title: this.$route.query.title || this.post?.title,
@@ -89,6 +141,8 @@ export default {
     return {
       item: null,
       damaku: null,
+      comments: [],
+      showPopup: false,
     };
   },
   computed: {
@@ -114,6 +168,10 @@ export default {
   },
   created() {
     this.getDetails();
+    this.getDanmaku();
+  },
+  beforeDestroy() {
+    this.danmaku?.destroy();
   },
   methods: {
     async getDetails() {
@@ -123,12 +181,6 @@ export default {
       }
 
       this.item = ret.item;
-
-      this.$nextTick(() => {
-        document.getElementById('js-video').onloadedmetadata = () => {
-          this.getDanmaku();
-        };
-      });
     },
     async getDanmaku() {
       const ret = await himawariSVC.getDanmaku(this.dougaId);
@@ -136,29 +188,41 @@ export default {
         return;
       }
 
-      this.danmaku?.destroy();
-      this.$nextTick(() => {
-        this.danmaku = new Danmaku({
-          engine: 'canvas',
-          container: document.getElementById('js-video-container'),
-          media: document.getElementById('js-video'),
-          comments: ret.items.map(item => {
-            return {
-              text: item.msg,
-              time: item.vpos_master / 100,
-              style: {
-                font: '16px sans-serif',
-                textAlign: 'start',
-                textBaseline: 'bottom',
-                direction: 'inherit',
-                fillStyle: '#fff',
-                strokeStyle: '#fff',
-                lineWidth: 1.0,
-              },
-            };
-          }),
-        });
+      this.comments = ret.items;
+
+      const newComments = this.comments.map(item => {
+        return {
+          text: item.msg,
+          time: item.vpos_master / 100,
+          // style: {
+          //   fontSize: '16px',
+          //   color: '#ffffff',
+          //   textShadow: '-1px -1px #000, -1px 1px #000, 1px -1px #000, 1px 1px #000',
+          // },
+          style: {
+            font: '16px sans-serif',
+            textAlign: 'start',
+            textBaseline: 'bottom',
+            direction: 'inherit',
+            fillStyle: '#fff',
+            strokeStyle: '#fff',
+            lineWidth: 1.0,
+          },
+        };
       });
+      if (!this.danmaku) {
+        this.$nextTick(() => {
+          this.danmaku = new Danmaku({
+            engine: 'canvas',
+            // engine: 'dom',
+            container: document.getElementById('js-video-container'),
+            media: document.getElementById('js-video'),
+            comments: newComments,
+          });
+        });
+      } else {
+        this.danmaku.comments = newComments;
+      }
     },
     openUrl(item) {
       window.liff.openWindow({
@@ -176,5 +240,40 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+.danmaku-table {
+  margin: 30px 0;
+  height: 100%;
+  overflow: hidden;
+  tbody {
+    border-top: 1px solid #ddd;
+    tr {
+      td {
+        border-bottom: 1px solid #ddd;
+        padding: 5px;
+      }
+    }
+  }
+}
+
+.scroller {
+  height: 100%;
+
+  &__item {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    border-bottom: 1px solid #ddd;
+
+    > div {
+      padding: 5px;
+    }
+
+    &__msg {
+      flex: 1;
+      line-height: 1.3;
+      font-size: 14px;
+    }
+  }
+}
 </style>
