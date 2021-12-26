6ef12ac53b7623e44f998d320ed35b2944353d0d
clipwww <clipwww@users.noreply.github.com>
2021-12-26 08:32:39 +0000

    deploy: 04f3d33721c764458e149da7cdb61e9c8c42092c

---
+++ precache-manifest.76a616b39c2c965b6191d8d13b8b2c3c.js
@@ -0,0 +1,258 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ccf3dbc57c20c5dea509626efd79e4c",
+    "url": "/liff-sp/404.html"
+  },
+  {
+    "revision": "6b4b8f88de2e5c769a95",
+    "url": "/liff-sp/css/app.e00a184d.css"
+  },
+  {
+    "revision": "c07a922545ea1ea4dd05",
+    "url": "/liff-sp/css/chunk-vendors.6d346ce5.css"
+  },
+  {
+    "revision": "dbbc1d3cb98466e85209",
+    "url": "/liff-sp/css/page-10~page-8.37405d12.css"
+  },
+  {
+    "revision": "25837c02e714470bd777",
+    "url": "/liff-sp/css/page-13.af3c6d8e.css"
+  },
+  {
+    "revision": "3eab455b205287e29af0",
+    "url": "/liff-sp/css/page-17.38dc5ed8.css"
+  },
+  {
+    "revision": "6922296d0d2182bc9f82",
+    "url": "/liff-sp/css/page-18.c18ba86b.css"
+  },
+  {
+    "revision": "093c9bd3f7e18e58f41a",
+    "url": "/liff-sp/css/page-2.b974cf0e.css"
+  },
+  {
+    "revision": "452a85913c0280e6aef1",
+    "url": "/liff-sp/css/page-20~page-23.b91cd82f.css"
+  },
+  {
+    "revision": "d26a5b1d9eeb57a2b458",
+    "url": "/liff-sp/css/page-21.7c85f952.css"
+  },
+  {
+    "revision": "390f893bbc0d3db79573",
+    "url": "/liff-sp/css/page-22.b91cd82f.css"
+  },
+  {
+    "revision": "8ca6813ae4e291912d9d",
+    "url": "/liff-sp/css/page-23.637395d5.css"
+  },
+  {
+    "revision": "23c5b25f49b15b8cf6f0",
+    "url": "/liff-sp/css/page-24.5adf2267.css"
+  },
+  {
+    "revision": "ecb4f8cc8300f9536df6",
+    "url": "/liff-sp/css/page-3.194d4b91.css"
+  },
+  {
+    "revision": "082210b5f63e3c2a5e52",
+    "url": "/liff-sp/css/page-4.e4bd0cb3.css"
+  },
+  {
+    "revision": "5d3b47faef0338645ca1",
+    "url": "/liff-sp/css/page-8.256dc6e6.css"
+  },
+  {
+    "revision": "c5f22787dfcdf3e1abbcb3d005cd11cd",
+    "url": "/liff-sp/images/fossils.png"
+  },
+  {
+    "revision": "4620e0206efd48a88c07aaa97fe064cb",
+    "url": "/liff-sp/images/himawari.png"
+  },
+  {
+    "revision": "a5e094e8b347b185091391c8b29aaa17",
+    "url": "/liff-sp/images/howhow.jpg"
+  },
+  {
+    "revision": "a305ccb8c0236f99786e3223fbc88399",
+    "url": "/liff-sp/images/komica.png"
+  },
+  {
+    "revision": "3840ed72fa8a2c5ddb6b8eb60e44a4a6",
+    "url": "/liff-sp/images/leaf.png"
+  },
+  {
+    "revision": "eeb7b040e612cab6b04d4ca4affa0c54",
+    "url": "/liff-sp/images/tenkinoko.jpeg"
+  },
+  {
+    "revision": "cd700040e15de57e3b1e633e07efc058",
+    "url": "/liff-sp/index.html"
+  },
+  {
+    "revision": "6b4b8f88de2e5c769a95",
+    "url": "/liff-sp/js/app.6b1db7b3.js"
+  },
+  {
+    "revision": "0715e2a88458b12797a5",
+    "url": "/liff-sp/js/chunk-2d0baa4f.fb598ced.js"
+  },
+  {
+    "revision": "714ec88fcef84052488f",
+    "url": "/liff-sp/js/chunk-2d0bd214.d2a6b5da.js"
+  },
+  {
+    "revision": "104f2d89c8fdd576ac79",
+    "url": "/liff-sp/js/chunk-2d0e13fd.44cac0d4.js"
+  },
+  {
+    "revision": "2d64763d0ad265eb9e0c",
+    "url": "/liff-sp/js/chunk-2d0e4c6f.9c763025.js"
+  },
+  {
+    "revision": "e29a5a63c02c5ee90948",
+    "url": "/liff-sp/js/chunk-a7b595c0.85e28890.js"
+  },
+  {
+    "revision": "bb48656b6c95bdb1fb00",
+    "url": "/liff-sp/js/chunk-e10c5c9a.e7dc509d.js"
+  },
+  {
+    "revision": "c07a922545ea1ea4dd05",
+    "url": "/liff-sp/js/chunk-vendors.471ce5ab.js"
+  },
+  {
+    "revision": "ed81f60a21edfed30a24",
+    "url": "/liff-sp/js/page-0.44f64a63.js"
+  },
+  {
+    "revision": "b86cbc927c7a0d86205e",
+    "url": "/liff-sp/js/page-1.41852520.js"
+  },
+  {
+    "revision": "a94ed5eff9ef28a281f6",
+    "url": "/liff-sp/js/page-10.72260839.js"
+  },
+  {
+    "revision": "ad3428064f09c2254f0b",
+    "url": "/liff-sp/js/page-10~page-11~page-12~page-13~page-15~page-16~page-17~page-18~page-2~page-21~page-22~page-24~page-~d0f31f1e.c601c187.js"
+  },
+  {
+    "revision": "115d9daa04bccf2b382f",
+    "url": "/liff-sp/js/page-10~page-20~page-22~page-23~page-24~page-6~page-8.5358050c.js"
+  },
+  {
+    "revision": "dbbc1d3cb98466e85209",
+    "url": "/liff-sp/js/page-10~page-8.5f2980d9.js"
+  },
+  {
+    "revision": "36817f83b134822586f2",
+    "url": "/liff-sp/js/page-11.7fc15dec.js"
+  },
+  {
+    "revision": "791e262c667ade819224",
+    "url": "/liff-sp/js/page-12.6cdad216.js"
+  },
+  {
+    "revision": "6428fbd0702440b615da",
+    "url": "/liff-sp/js/page-12~page-15~page-16~page-17~page-18~page-22.67ebcb92.js"
+  },
+  {
+    "revision": "25837c02e714470bd777",
+    "url": "/liff-sp/js/page-13.0654f337.js"
+  },
+  {
+    "revision": "2197558b3496086f8359",
+    "url": "/liff-sp/js/page-14.003324f7.js"
+  },
+  {
+    "revision": "b032b5e57bdc23a8469e",
+    "url": "/liff-sp/js/page-15.10644dde.js"
+  },
+  {
+    "revision": "4e2d26056443c38d2f9d",
+    "url": "/liff-sp/js/page-16.fae0f06b.js"
+  },
+  {
+    "revision": "3eab455b205287e29af0",
+    "url": "/liff-sp/js/page-17.583026f4.js"
+  },
+  {
+    "revision": "6922296d0d2182bc9f82",
+    "url": "/liff-sp/js/page-18.5f22ce8f.js"
+  },
+  {
+    "revision": "c8f79c7217abe0ecc014",
+    "url": "/liff-sp/js/page-19.a01ed6d0.js"
+  },
+  {
+    "revision": "093c9bd3f7e18e58f41a",
+    "url": "/liff-sp/js/page-2.ae5be445.js"
+  },
+  {
+    "revision": "5d0cf400c78013abd00e",
+    "url": "/liff-sp/js/page-20.f88a04f7.js"
+  },
+  {
+    "revision": "452a85913c0280e6aef1",
+    "url": "/liff-sp/js/page-20~page-23.d73a14fe.js"
+  },
+  {
+    "revision": "d26a5b1d9eeb57a2b458",
+    "url": "/liff-sp/js/page-21.89dc66e9.js"
+  },
+  {
+    "revision": "390f893bbc0d3db79573",
+    "url": "/liff-sp/js/page-22.e3d36fbd.js"
+  },
+  {
+    "revision": "8ca6813ae4e291912d9d",
+    "url": "/liff-sp/js/page-23.e99a6a89.js"
+  },
+  {
+    "revision": "23c5b25f49b15b8cf6f0",
+    "url": "/liff-sp/js/page-24.3e436f52.js"
+  },
+  {
+    "revision": "0dc7ceeba76bbb47d0a2",
+    "url": "/liff-sp/js/page-2~page-20~page-22~page-23.e87862ce.js"
+  },
+  {
+    "revision": "ecb4f8cc8300f9536df6",
+    "url": "/liff-sp/js/page-3.97fa19f3.js"
+  },
+  {
+    "revision": "082210b5f63e3c2a5e52",
+    "url": "/liff-sp/js/page-4.1621f9b6.js"
+  },
+  {
+    "revision": "324fa47f069163faf8d7",
+    "url": "/liff-sp/js/page-5.96eecd39.js"
+  },
+  {
+    "revision": "80d252279ed5c83f8cbf",
+    "url": "/liff-sp/js/page-6.be92d7d9.js"
+  },
+  {
+    "revision": "5d3b47faef0338645ca1",
+    "url": "/liff-sp/js/page-8.99b90d5a.js"
+  },
+  {
+    "revision": "d3f697478384217b8a48",
+    "url": "/liff-sp/js/page-9.41ebbee7.js"
+  },
+  {
+    "revision": "bb059416b9be52a3dbf17e68d03f247b",
+    "url": "/liff-sp/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/liff-sp/robots.txt"
+  },
+  {
+    "revision": "36fcc3d75552bb41877e562608655323",
+    "url": "/liff-sp/sw.js"
+  }
+]);
---
+++ sw.js
@@ -0,0 +1,34 @@
+/* eslint-disable-next-line no-redeclare */
+/* global self */
+
+// This service worker file is effectively a 'no-op' that will reset any
+// previous service worker registered for the same host:port combination.
+
+// It is read and returned by a dev server middleware that is only loaded
+// during development.
+
+// In the production build, this file is replaced with an actual service worker
+// file that will precache your site's local assets.
+
+self.addEventListener('push', event => {
+  if (!event.data) return;
+
+  const options = event.data.json();
+  const title = options.title;
+  event.waitUntil(
+    self.registration.showNotification(title, options)
+  )
+})
+
+self.addEventListener('notificationclick', event => {
+  
+  event.notification.close();
+
+  const notification = event.notification;
+  const action = event.action;
+  console.log(notification, action)
+  if (!notification.data) return;
+  event.waitUntil(
+    clients.openWindow(notification.data.url)
+  )
+})
